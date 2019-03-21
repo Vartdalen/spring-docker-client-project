29ddf4ac0b6248e7702aad8454c08ddb07ce1a2d
vartdalen <viktor.v.johansen@hotmail.com>
2019-03-21 16:59:39 +0100

    REMOVE useless configuration annotation in securityconfig

--- src/main/java/no/oslomet/s315615springdockerclientproject/config/SecurityConfig.java
+++ src/main/java/no/oslomet/s315615springdockerclientproject/config/SecurityConfig.java
@@ -3,7 +3,6 @@ package no.oslomet.s315615springdockerclientproject.config;
 import no.oslomet.s315615springdockerclientproject.service.LoginService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -12,7 +11,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
-@Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
